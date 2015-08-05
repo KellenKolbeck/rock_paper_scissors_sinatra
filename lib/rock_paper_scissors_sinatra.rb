a6e540347b5ba39f554077b63c52775ff24aa737
Kellen Kolbeck & Kendra Ash <student@epicodus.com>
2015-08-05 16:42:11 -0700

    Adding random generator to computer's choice

--- lib/rock_paper_scissors_sinatra.rb
+++ lib/rock_paper_scissors_sinatra.rb
@@ -3,7 +3,7 @@ class String
     output_check = {"rock" => 0, "paper" => 1, "scissors" => 2}
 # Assuming computer entered rock for each turn
     user_input=output_check[self]
-    computer_input=0
+    computer_input=rand(0..2)
     if user_input==computer_input
       "tie"
     elsif user_input==0 && computer_input==2 || user_input==1 && computer_input==0 || user_input==2 && computer_input==1
